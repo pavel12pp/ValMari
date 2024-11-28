dcdce303f8e00af1ab911f277a7f66c78844f53b
Pavel Komandyshko <pavelkom12@yandex.ru>
2024-11-28 20:12:11 +0300

    Add animation

--- src/script/component/mixin/header.js
+++ src/script/component/mixin/header.js
@@ -2,6 +2,17 @@ let body = document.querySelector('body');
 let burgerOpen = document.querySelector('#burger-open');
 let burgerClose = document.querySelector('#burger-close');
 
+let wrapper = document.querySelector('.wrapper');
+
 burgerOpen.addEventListener('click', (event) => {body.classList.add('burger-active')});
 
 burgerClose.addEventListener('click', (event) => {body.classList.remove('burger-active')});
+
+wrapper.addEventListener('scroll', (event) => {
+  let nowScrollTop = wrapper.scrollTop;
+  if (nowScrollTop > 0) {
+    wrapper.classList.add('scrolling');
+  } else {
+    wrapper.classList.remove('scrolling');
+  }
+})
