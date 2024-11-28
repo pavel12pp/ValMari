8108532e540ff1c98a3c30221638a6a8d5484359
Pavel Komandyshko <pavelkom12@yandex.ru>
2024-11-28 16:37:50 +0300

    Full blocks

---
+++ src/script/component/page/index/master.js
@@ -0,0 +1,36 @@
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+
+new Swiper('.master-slider', {
+  modules: [Navigation, Pagination],
+  navigation: {
+    nextEl: ".master__button-next",
+    prevEl: ".master__button-prev",
+  },
+  grabCursor: true,
+  pagination: {
+    el: ".master__pagination",
+    clickable: true,
+    bulletClass: 'master__pagination-bullet',
+    bulletActiveClass: "active",
+    renderBullet: function (index, className) {
+      return `<span class="master__pagination-bullet ${className}"></span>`;
+    },
+  },
+  slidesPerView: 1,
+  spaceBetween: 20,
+  slideActiveClass: "active",
+  watchSlidesProgress: true,
+  slideVisibleClass: 'visible',
+  breakpoints: {
+
+    700: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1200: {
+      spaceBetween: 153,
+      slidesPerView: 2,
+    },
+  },
+}) 
--- src/script/source/index.js
+++ src/script/source/index.js
@@ -2,3 +2,4 @@ import "./../component/mixin/header.js";
 import "./../component/page/index/course.js";
 import "./../component/page/index/map.js";
 import "./../component/page/index/stock.js";
+import "./../component/page/index/master.js";
